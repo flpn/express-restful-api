9d8c5f7f2bfbe519f71554a4afff423b1c9b3c0c
Felipe Netto <flpncc@gmail.com>
2018-06-24 19:30:12 -0300

    Default loging and error handling

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
 
 const productRouter = require('./api/routes/products')
 const orderRouter = require('./api/routes/orders')
 
+const app = express()
+
+app.use(morgan('dev'))
+
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
+
+app.use((req, res, next) => {
+    const error = new Error('URL not found!')
+    error.status = 404
+    
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
