38782f4886e93658edc4bac5ccf6fa7affa2de16
Felipe Netto <flpncc@gmail.com>
2018-06-25 22:17:08 -0300

    Mongoose validation and improving response for the products router

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,11 +5,29 @@ const Product = require('../models/product')
 
 const router = express.Router()
 
+const fullPath = 'http://localhost:3000/api/products'
+
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            res.status(200).json(docs)
+            let response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: fullPath + doc._id
+                        }
+                    }
+                })
+            }
+
+            res.status(200).json(response)
         })
         .catch(err => {
             res.status(500).json({
@@ -29,7 +47,15 @@ router.post('/', (req, res, next) => {
         .then(result => {
             res.status(200).json({
                 message: 'Product created!',
-                product: result
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: fullPath + result._id
+                    }
+                }
             })
         })
         .catch(err => {
@@ -43,6 +69,7 @@ router.get('/:productId', (req, res, next) => {
     let id = req.params.productId
 
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             if(doc)
@@ -68,7 +95,13 @@ router.put('/:productId', (req, res, next) => {
     Product.update({_id: id}, {$set: updatedProperties})
         .exec()
         .then(result => {
-            res.status(200).json(result)
+            res.status(200).json({
+                message: 'Product updated!',
+                request: {
+                    type: 'GET',
+                    url: fullPath + id
+                }
+            })
         })
         .catch(err => {
             res.status(500).json({
@@ -83,7 +116,9 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({_id: id})
         .exec()
         .then(result => {
-            res.status(200).json(result)
+            res.status(200).json({
+                message: `Product ${id} deleted!`
+            })
         })
         .catch(err => {
             res.status(500).json({
