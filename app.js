a7217aaf21e0c9c5039b6e0bf264e27647dfbae0
Felipe Netto <flpncc@gmail.com>
2018-07-01 19:13:11 -0300

    Handling CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const productRouter = require('./api/routes/products')
 const orderRouter = require('./api/routes/orders')
@@ -14,22 +15,7 @@ mongoose.Promise = global.Promise
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
-// Handling CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Headers',
-         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    )
-
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json()
-    }
-
-    next()
-})
+app.use(cors())
 
 app.use('/uploads', express.static('uploads'))
 app.use('/api/products', productRouter)
